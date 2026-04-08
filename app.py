14b3e38042536609c874f7584039345f5a4c2d1b
lenacvetkovic9 <cvetkoviclena9@gmail.com>
2026-04-08 16:35:32 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,24 @@
+import os
+import sqlite3
+from flask import Flask, request, jsonify, json
+from flask_cors import CORS
+
+app = Flask(_name_)
+@app.route("/")
+def index():
+  return "Zdravo brate"
+@app.route("/primer-string")
+def string():
+  return "Neki ne preterano dugacak text"
+@app.route("/primer-broj")
+def broj():
+  return 265
+@app.route("/primer-niz")
+def niz():
+  nekiNiz=[1,2,3,4,5]
+@ap.route("/primer-json)
+def primerJson():
+  data={
+    "message": "This is a JSON response",
+    "status": "sussess"
+  }
